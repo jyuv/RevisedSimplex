3d5fbd827cf9a9b8176e7d64d40c82f4212eaf4f
yuvi <jacobyyuval@gmail.com>
2021-05-13 10:32:41 +0300

    added initial performance checks

---
+++ performance_measurement.py
@@ -0,0 +1,56 @@
+import time
+from dataclasses import dataclass
+import numpy as np
+from test_simplex import generate_random_case
+from Simplex import Simplex
+from scipy.optimize import linprog
+
+CASES_AMOUNT = 1000
+MAX_M = 15
+MAX_N = 10
+MIN_INT_ELEMENT = -2147483648
+MAX_INT_ELEMENT = 2147483647
+
+SIMPLEX_SOLVER = Simplex()
+
+FUNCS_TO_ANALYZE = {"ours full simplex": SIMPLEX_SOLVER.get_optimal_solution,
+                    "ours feasible check": SIMPLEX_SOLVER.has_feasible_solution,
+                    "scipy's revised simplex": lambda a, b, c: linprog(
+    A_ub=a, b_ub=b, c=-c, method="revised simplex")}
+
+
+@dataclass
+class TestCase(object):
+    A: np.ndarray
+    b: np.ndarray
+    c: np.ndarray
+
+
+def get_random_cases(cases_amount: int = CASES_AMOUNT):
+    cases = []
+    for case in range(cases_amount):
+        A, b, c = generate_random_case(MAX_M, MAX_M,
+                                       MIN_INT_ELEMENT, MAX_INT_ELEMENT)
+        cases.append(TestCase(A, b, c))
+    return cases
+
+
+def measure_time(test_cases, func):
+    start_time = time.time()
+    for case in test_cases:
+        func(case.A, case.b, case.c)
+    return time.time() - start_time
+
+
+def compare_performance():
+    cases = get_random_cases()
+    performances = {}
+    for func_desc, func in FUNCS_TO_ANALYZE.items():
+        performances[func_desc] = measure_time(cases, func)
+
+    for func_desc, duration in performances.items():
+        print("The duration of running {0}: {1}".format(func_desc, duration))
+
+
+if __name__ == "__main__":
+    compare_performance()
--- test_simplex.py
+++ test_simplex.py
@@ -1,10 +1,12 @@
 import math
 import random
+import time
+
 import numpy as np
 from scipy.optimize import linprog
 from Simplex import Simplex, ResultCode
 
-RANDOM_CASES = 100
+RANDOM_CASES = 1000
 MAX_M = 15
 MAX_N = 10
 MIN_INT_ELEMENT = -2147483648
@@ -17,14 +19,13 @@ LINPROG_RCODE_TO_RCODE = {0: ResultCode.FINITE_OPTIMAL,
 
 SIMPLEX_SOLVER = Simplex()
 
-# Todo: Add performance Tests
 
-
-def generate_random_case():
-    m, n = [random.randint(1, end) for end in (MAX_M, MAX_N)]
-    A = np.random.randint(MIN_INT_ELEMENT, MAX_INT_ELEMENT, size=(m, n))
-    b = np.random.randint(MIN_INT_ELEMENT, MAX_INT_ELEMENT, size=m)
-    c = np.random.randint(MIN_INT_ELEMENT, MAX_INT_ELEMENT, size=n)
+def generate_random_case(max_m=MAX_M, max_n=MAX_N,
+                         min_int=MIN_INT_ELEMENT, max_int=MAX_INT_ELEMENT):
+    m, n = [random.randint(1, end) for end in (max_m, max_n)]
+    A = np.random.randint(min_int, max_int, size=(m, n))
+    b = np.random.randint(min_int, max_int, size=m)
+    c = np.random.randint(min_int, max_int, size=n)
 
     return A, b, c
 
@@ -74,3 +75,41 @@ def test_random_cases():
             assert expected_res_code == res.res_code
             if expected_res_code == ResultCode.FINITE_OPTIMAL:
                 assert math.isclose(expected_res.fun, -res.optimal_score)
+
+# Todo: move performance check into performance measurment when differences
+#       figured out
+
+
+def compare_performance():
+    A_list, b_list, c_list = [], [], []
+    for case in range(RANDOM_CASES):
+        A, b, c = generate_random_case()
+        A_list.append(A)
+        b_list.append(b)
+        c_list.append(c)
+
+    cur_start_time = time.time()
+    for case in range(RANDOM_CASES):
+        A, b, c = A_list[case], b_list[case], c_list[case]
+        SIMPLEX_SOLVER.get_optimal_solution(A, b, c)
+    time_ours_full = time.time() - cur_start_time
+
+    cur_start_time = time.time()
+    for case in range(RANDOM_CASES):
+        A, b, c = A_list[case], b_list[case], c_list[case]
+        linprog(A_ub=A, b_ub=b, c=-c, method="revised simplex")
+    time_scipy = time.time() - cur_start_time
+
+    cur_start_time = time.time()
+    for case in range(RANDOM_CASES):
+        A, b, c = A_list[case], b_list[case], c_list[case]
+        SIMPLEX_SOLVER.has_feasible_solution(A, b, c)
+    time_ours_feasible = time.time() - cur_start_time
+
+    print("The time of scipy's linprog: {0}".format(time_scipy))
+    print("The time of ours full simplex: {0}".format(time_ours_full))
+    print("The time of ours feasible simplex: {0}".format(time_ours_feasible))
+
+
+if __name__ == "__main__":
+    compare_performance()
